36301901d10e0ef765daea31eae68045eb8add15
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:18:48 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,29 +1,30 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{analog_in_v3_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{analog_in_v3_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet 3.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet 3.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let analog_in_v3_bricklet = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ai = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for voltage events.
-    let voltage_listener = analog_in_v3_bricklet.get_voltage_receiver();
-    // Spawn thread to handle received events. This thread ends when the analog_in_v3_bricklet
+    // Create receiver for voltage events.
+    let voltage_receiver = ai.get_voltage_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `ai` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in voltage_listener {
-            println!("Voltage: {}{}", event as f32 / 1000.0, " V");
+        for voltage in voltage_receiver {
+            println!("Voltage: {} V", voltage as f32 / 1000.0);
         }
     });
 
-    // Set period for voltage callback to 1s (1000ms) without a threshold
-    analog_in_v3_bricklet.set_voltage_callback_configuration(1000, false, 'x', 0, 0);
+    // Set period for voltage callback to 1s (1000ms) without a threshold.
+    ai.set_voltage_callback_configuration(1000, false, 'x', 0, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{analog_in_v3_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{analog_in_v3_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet 3.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Analog In Bricklet 3.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let analog_in_v3_bricklet = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let ai = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current voltage
-    let voltage = analog_in_v3_bricklet.get_voltage().recv()?;
-    println!("Voltage: {}{}", voltage as f32 / 1000.0, " V");
+    // Get current voltage.
+    let voltage = ai.get_voltage().recv()?;
+    println!("Voltage: {} V", voltage as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();

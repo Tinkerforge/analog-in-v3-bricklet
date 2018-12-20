b22b898370cadd2378c27f7cf360494c29baec5f
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:51:05 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  analog_in_v3_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{analog_in_v3_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,21 +10,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ai = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let voltage_receiver = ai.get_voltage_callback_receiver();
+    let voltage_receiver = ai.get_voltage_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `ai` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for voltage in voltage_receiver {           
-                		println!("Voltage: {} V", voltage as f32 /1000.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `ai` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for voltage in voltage_receiver {
+            println!("Voltage: {} V", voltage as f32 / 1000.0);
+        }
+    });
 
-		// Set period for voltage callback to 1s (1000ms) without a threshold.
-		ai.set_voltage_callback_configuration(1000, false, 'x', 0, 0);
+    // Set period for voltage callback to 1s (1000ms) without a threshold.
+    ai.set_voltage_callback_configuration(1000, false, 'x', 0, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  analog_in_v3_bricklet::*};
-
+use tinkerforge::{analog_in_v3_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ai = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current voltage.
-let voltage = ai.get_voltage().recv()?;
-		println!("Voltage: {} V", voltage as f32 /1000.0);
+    // Get current voltage.
+    let voltage = ai.get_voltage().recv()?;
+    println!("Voltage: {} V", voltage as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();

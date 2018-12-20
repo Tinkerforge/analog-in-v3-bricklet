df50b451f3965c65b42f94c94cc824927f47b391
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:31:33 +0100

    rust: Reformat examples.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{analog_in_v3_bricklet::*, ip_connection::IpConnection};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  analog_in_v3_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,22 +13,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ai = AnalogInV3Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let voltage_receiver = ai.get_voltage_callback_receiver();
+     let voltage_receiver = ai.get_voltage_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `ai` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for voltage in voltage_receiver {
-            println!("Voltage: {} V", voltage as f32 / 1000.0);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `ai` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for voltage in voltage_receiver {           
+                		println!("Voltage: {} V", voltage as f32 /1000.0);
+            }
+        });
 
-    // Configure threshold for voltage "smaller than 5 V"
-    // with a debounce period of 1s (1000ms).
-    ai.set_voltage_callback_configuration(1000, false, '<', 5 * 1000, 0);
+		// Configure threshold for voltage "smaller than 5 V"
+		// with a debounce period of 1s (1000ms).
+		ai.set_voltage_callback_configuration(1000, false, '<', 5*1000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();

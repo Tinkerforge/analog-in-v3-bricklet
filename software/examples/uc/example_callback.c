24a0be140a7de73a61d8dfc5fcad5c84ae3f71b7
Erik Fleckstein <erik@tinkerforge.com>
2020-08-19 16:50:04 +0200

    Add uc examples.

---
+++ software/examples/uc/example_callback.c
@@ -0,0 +1,40 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_analog_in_v3.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Analog In Bricklet 3.0
+
+void check(int rc, const char* msg);
+
+
+
+// Callback function for voltage callback
+void voltage_handler(TF_AnalogInV3 *device, uint16_t voltage, void *user_data) {
+	(void)device; (void)user_data; // avoid unused parameter warning
+
+	tf_hal_printf("Voltage: %d 1/%d V\n", voltage, 1000.0);
+}
+
+
+TF_AnalogInV3 ai;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_analog_in_v3_create(&ai, UID, hal), "create device object");
+
+
+	// Register voltage callback to function voltage_handler
+	tf_analog_in_v3_register_voltage_callback(&ai,
+	                                         voltage_handler,
+	                                         NULL);
+
+	// Set period for voltage callback to 1s (1000ms) without a threshold
+	tf_analog_in_v3_set_voltage_callback_configuration(&ai, 1000, false, 'x', 0, 0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}
---
+++ software/examples/uc/example_simple.c
@@ -0,0 +1,31 @@
+
+
+#include "bindings/hal_common.h"
+#include "bindings/bricklet_analog_in_v3.h"
+
+#define UID "XYZ" // Change XYZ to the UID of your Analog In Bricklet 3.0
+
+void check(int rc, const char* msg);
+
+
+
+
+TF_AnalogInV3 ai;
+
+void example_setup(TF_HalContext *hal) {
+	// Create device object
+	check(tf_analog_in_v3_create(&ai, UID, hal), "create device object");
+
+
+	// Get current voltage
+	uint16_t voltage;
+	check(tf_analog_in_v3_get_voltage(&ai, &voltage), "get voltage");
+
+	tf_hal_printf("Voltage: %d 1/%d V\n", voltage, 1000.0);
+
+}
+
+void example_loop(TF_HalContext *hal) {
+	// Poll for callbacks
+	tf_hal_callback_tick(hal, 0);
+}

7850f0024d74086bb7bdb524915c5b5b73e016ad
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 11:03:12 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_analog_in_v3.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_analog_in_v3.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Analog In Bricklet 3.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for voltage callback
 static void voltage_handler(TF_AnalogInV3 *device, uint16_t voltage, void *user_data) {
@@ -24,7 +20,7 @@ static TF_AnalogInV3 ai;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_analog_in_v3_create(&ai, UID, hal), "create device object");
+	check(tf_analog_in_v3_create(&ai, NULL, hal), "create device object");
 
 	// Register voltage callback to function voltage_handler
 	tf_analog_in_v3_register_voltage_callback(&ai,
--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_analog_in_v3.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_analog_in_v3.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Analog In Bricklet 3.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_AnalogInV3 ai;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_analog_in_v3_create(&ai, UID, hal), "create device object");
+	check(tf_analog_in_v3_create(&ai, NULL, hal), "create device object");
 
 	// Get current voltage
 	uint16_t voltage;
